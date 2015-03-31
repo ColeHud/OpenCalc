b0d473abda5bda9da011b971012466840e9b99d7
ColeHud <cmhudson11@gmail.com>
2015-03-30 22:13:05 -0400

    Finished presentation at stem expo

--- Code/main1.py
+++ Code/main1.py
@@ -19,6 +19,7 @@ import RPi.GPIO as GPIO
 
 #set up the device for emitting keystrokes
 device = uinput.Device([
+		uinput.KEY_0,
         uinput.KEY_1,
         uinput.KEY_2,
         uinput.KEY_3,
@@ -36,13 +37,14 @@ device = uinput.Device([
         uinput.KEY_LEFTBRACE,
         uinput.KEY_RIGHTBRACE,
         uinput.KEY_NUMERIC_STAR,
+        uinput.KEY_EQUAL,
         ])
 
 #GPIO Mapping
 mapping = {
 	7: uinput.KEY_0,
 	8: uinput.KEY_LEFTSHIFT,
-	10: uinput.KEY_ENTER,
+	#10: uinput.KEY_ENTER,
 	#11 uinput.KEY_,
 	12: uinput.KEY_1,
 	13: uinput.KEY_2,
@@ -51,11 +53,11 @@ mapping = {
 	18: uinput.KEY_4,
 	19: uinput.KEY_5,
 	21: uinput.KEY_6,
-	#22: uinput.KEY_5,
+	22: uinput.KEY_NUMERIC_STAR,
 	23: uinput.KEY_7,
 	24: uinput.KEY_8,
 	29: uinput.KEY_9,
-	33: uinput.KEY_BACKSLASH,
+	33: uinput.KEY_SLASH,
 	#31: uinput.KEY_8,
 	#32: uinput.KEY_9,
 	#33: uinput.KEY_BACKSLASH,
@@ -75,6 +77,10 @@ for pin in mapping:
 	#setup each pin
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#setup the combos
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 #program loop
 while 1:
 	#for each pin in the mapping dictionary
@@ -86,6 +92,41 @@ while 1:
 			#do nothing until the button is released
 			while GPIO.input(pin) == 0:
 				time.sleep(.1)
+
+	#combos
+
+	#+
+	if(GPIO.input(11) == 0):
+		device.emit_combo([
+        	uinput.KEY_LEFTSHIFT,
+        	uinput.KEY_EQUAL,
+        ])
+        #do nothing until the button is released
+		while GPIO.input(11) == 0:
+			time.sleep(.1)
+
+
+	'''
+	#*
+	if(GPIO.input(22)):
+		device = uinput.Device([
+        	uinput.KEY_LEFTSHIFT,
+        	uinput.KEY_8,
+        ])
+	'''
+
+	#enter
+	if(GPIO.input(10) == 0):
+		device.emit_combo([
+        	uinput.KEY_LEFTSHIFT,
+        	uinput.KEY_ENTER,
+        ])
+        #do nothing until the button is released
+		while GPIO.input(10) == 0:
+			time.sleep(.1)
+
+
+
 
 
 #cleanup the GPIO
